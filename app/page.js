012167741a1d2c10cda3ad1e360b2c55a574f87e
Ameer-m <ameerahmedkhan.m@gmail.com>
2024-04-20 12:29:29 +0400

    Home page Update

--- app/page.js
+++ app/page.js
@@ -40,40 +40,49 @@ export default function Home() {
   return (
     <>
       <MyModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
-      <section className="heroImg flex items-center justify-center w-full">
-        <div className="flex flex-col justify-between items-center lg:flex-row w-full px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-          <div className="mb-12 lg:max-w-lg lg:pr-5 lg:mb-0 w-full">
-            <div className="max-w-xl mb-6">
-              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white lg:text-5xl sm:leading-none">
-                Armani Beach{' '}
-                <br className="hidden md:block" />
-                Residences at Palm{" "}
-                <span className="inline-block text-deep-purple-accent-400">
-                  Jumeirah by Arada
-                </span>
-              </h2>
-              <div>
-                <span className="inline text-lg md:text-xl">Starting From</span>
-                <p className="text-3xl lg:text-4xl font-bold">AED 21,500,000</p>
-              </div>
+      <section className={`heroImg flex items-center justify-center w-full`}>
+      <Image
+        src="/palm-armani.webp"
+        alt="Palm Armani"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        quality={100}
+        className="absolute z-0"
+      />
+      <div className="overlay absolute inset-0 z-10"></div>
+      <div className="flex flex-col justify-between items-center lg:flex-row w-full px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 relative z-20">
+        <div className="mb-12 lg:max-w-lg lg:pr-5 lg:mb-0 w-full">
+          <div className="max-w-xl mb-6">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white lg:text-5xl sm:leading-none">
+              Armani Beach{' '}
+              <br className="hidden md:block" />
+              Residences at Palm <span className="inline-block text-deep-purple-accent-400">
+                Jumeirah by Arada
+              </span>
+            </h2>
+            <div>
+              <span className="inline text-lg md:text-xl">Starting From</span>
+              <p className="text-3xl lg:text-4xl font-bold">AED 21,500,000</p>
             </div>
-          </div>
-          <div className="bg-black/40 rounded-3xl p-8 flex flex-col md:ml-auto mt-10 md:mt-0 border-4 border-gray-300">
-            <div className="mb-5 border-b pb-2 border-white">
-              <h2 className="text-gray-50 text-lg md:text-2xl font-medium title-font capitalize">
-                Please fill in the form
-              </h2>
-              <p className="text-gray-50">
-                Begin your journey to premium living by sharing your details with us.
-              </p>
-            </div>
-            {/* Lazy load the form component */}
-            <React.Suspense fallback={<div>Loading...</div>}>
-              <MyForm />
-            </React.Suspense>
           </div>
         </div>
-      </section>
+        <div className="bg-black/40 rounded-3xl p-8 flex flex-col md:ml-auto mt-10 md:mt-0 border-4 border-gray-300">
+          <div className="mb-5 border-b pb-2 border-white">
+            <h2 className="text-gray-50 text-lg md:text-2xl font-medium title-font capitalize">
+              Please fill in the form
+            </h2>
+            <p className="text-gray-50">
+              Begin your journey to premium living by sharing your details with us.
+            </p>
+          </div>
+          <React.Suspense fallback={<div>Loading...</div>}>
+            {/* Lazy load the form component */}
+            <MyForm />
+          </React.Suspense>
+        </div>
+      </div>
+    </section>
 
       <section>
          <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
